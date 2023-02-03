3523b1823ae200aa2e3d7955a406d3ff0e330272
Jsh0227 <Jsh0227@aol.com>
2023-02-03 18:21:57 -0500

    created html, css

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const startButton =document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+
+//starts quiz
+
+function startQuiz(){
+    console.log('Started')
+    startButton.classlist.add('hide')
+    questionContainterElement.classlist.remove('hide')
+
+}
+
+//Sets the next question
+function setNextQuestion(){
+
+
+}
+
+//Selects the answer
+function selectAnswer(){
+
+
+}
