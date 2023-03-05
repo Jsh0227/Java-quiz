a1a6a40371939c44603c510e039134ebb0e8a9dc
Jsh0227 <Jsh0227@aol.com>
2023-03-05 18:36:52 -0500

    changes timer to subtract when quiz starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,113 @@
 const startButton = document.getElementById('start-btn')
-const instructGone= document.getElementById('instructions')
+const instructGone = document.getElementById('instructions')
 const showQuestionsEl = document.getElementById('question-container')
 startButton.addEventListener('click', startQuiz)
 instructGone.addEventListener('click', startQuiz)
 const questionEl = document.getElementById('question')
 const answerButtons = document.getElementById('answer-buttons')
 const countdownEl = document.getElementById('countdown')
-let timeLeft = 150;
-let timerId;
+const submitEl = document.getElementById('submit')
+const initialsEl = document.getElementById('intials')
 
+var timeLeft = 60;
 //starts quiz
-let startQuestions=""
+let startQuestions = ""
 var index = 0
-function startQuiz(){
-    console.log('Started')
+function startQuiz() {
     startButton.classList.add('hide')
     instructGone.classList.add('hide')
     showQuestionsEl.classList.remove('hide')
-    timerId = setInterval(timerFunc , 1000)
-    countdownEl.textContent = `${timeLeft} seconds`
-   setNextQuestion()
+    timerId = setInterval(timerFunc, 1000)
+    setNextQuestion()
 }
-function timerFunc(){
-    timeLeft--;
-        countdownEl.textContent = `${timeLeft} seconds`
-        if (timeLeft <= 0){
-            gameOver()
-        }
+function timerFunc() {
+    countdownEl.textContent = `${timeLeft--} seconds`
 }
-function setNextQuestion(){
+
+function setNextQuestion() {
     var currentQuestion = questions[index]
-    questionEl.textContent= currentQuestion.question
+    questionEl.textContent = currentQuestion.question
     var answer1 = document.getElementById("answer-1")
+    var answer2 = document.getElementById("answer-2")
+    var answer3 = document.getElementById("answer-3")
+    var answer4 = document.getElementById("answer-4")
     answer1.textContent = questions[index].answers[0]
+    answer2.textContent = questions[index].answers[1]
+    answer3.textContent = questions[index].answers[2]
+    answer4.textContent = questions[index].answers[3]
     answer1.addEventListener('click', selectAnswer)
+    answer2.addEventListener('click', selectAnswer)
+    answer3.addEventListener('click', selectAnswer)
+    answer4.addEventListener('click', selectAnswer)
 }
 
 //Selects the answer
-function selectAnswer(event){
-console.log(event.target)
+function selectAnswer(event) {
+    console.log(event.target)
 
-//what caused the event listener to run(event.target)  (text-content-calls it to run)
-var usersChoice = event.target.textContent
-var correctChoice = questions[index].correct
-var resultEl = document.getElementById("result-container")
-    if (correctChoice === usersChoice){
-            resultEl.textContent = 'CORRECT';
-        }else{
-            resultEl.textContent = 'Incorrect';
+    //what caused the event listener to run(event.target)  (text-content-calls it to run)
+    var usersChoice = event.target.textContent
+    var correctChoice = questions[index].correct
+    var resultEl = document.getElementById("result-container")
+    if (correctChoice === usersChoice) {
+        resultEl.textContent = 'CORRECT';
+    } else {
+        resultEl.textContent = 'Incorrect';
+    }
+    resultEl.removeAttribute("class");
+
+    //Delays the function.  Goes to the next question or produces GameOver
+    setTimeout(function () {
+        resultEl.setAttribute("class", "hide");
+        if (index === questions.length || timeLeft < 0) {
+            gameOver()
         }
-            resultEl.removeAttribute ("class");
-        setTimeout(function() {
-            resultEl.setAttribute ("class" , "hide");
-        }, 1000)
+            else {
+                setNextQuestion()
+            }
+        
+    }, 500)
     index++
 
-    if( index === questions.length || timeLeft < 0){
-        gameOver()
-    }
-
-    setNextQuestion()
 }
-    function gameOver(){
-        clearInterval(timerId)
+function gameOver() {
+    timeLeft = timeLeft + 1;
+    clearInterval(timerId)
 
-        let endContainerEl = document.getElementById('end-container')
-            endContainerEl.removeAttribute('class')
-        let finalEl = document.getElementById('your-score');
-            finalEl.textContent = timeLeft;
-            showQuestionsEl.setAttribute('class' , 'hide')
-    }
-const questions =[
-{
-    question: "what is 2 + 2?",
-    answers: ['4','44', '33', '2'], 
-    correct: "4"
-},
-{
-    question: "What is 3 + 3?",
-    answers:['6', '8', '3', '17'],
-    correct: "6"
+    let endContainerEl = document.getElementById('end-container')
+    endContainerEl.removeAttribute('class')
+    let finalEl = document.getElementById('your-score');
+    finalEl.textContent = timeLeft;
+    showQuestionsEl.setAttribute('class', 'hide')
 }
+
+submitEl.onclick = storePlayerScore;
+
+function storePlayerScore() {
+    let initials = initialsEl.value.trim();
+    if (initials !== "") {
+        let highScores = JSON.parse(localStorage.getItem("high-scores")) || [];
+        let playerScore = {
+            score: timeLeft,
+            initials: initials,
+        };
+        highScores.push(playerScore);
+        localStorage.setItem("high-scores", JSON.stringify(highScores))
+        //window.location.href = "highScores.html"
+    }
+}
+
+const questions = [
+    {
+        question: "what is 2 + 2?",
+        answers: ['4', '44', '33', '2'],
+        correct: "4"
+    },
+    {
+        question: "What is 3 + 3?",
+        answers: ['6', '8', '3', '17'],
+        correct: "6"
+    }
 ]
 
 //timer
